caf93c27f75bc7b97f6618f1fa50642f9bc57959
Taneeshq2002 <119059928+Taneeshq2002@users.noreply.github.com>
2024-10-30 21:56:44 +0530

    created client side app and connected it to blockchain using metamask. Also added tests

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -16,10 +16,10 @@ contract TodoList {
     bool completed
   );
 
-  // event TaskCompleted(
-  //   uint id,
-  //   bool completed
-  // );
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
 
   constructor() public {
     createTask("Check out dappuniversity.com");
@@ -30,4 +30,12 @@ contract TodoList {
     tasks[taskCount] = Task(taskCount, _content, false);
     emit TaskCreated(taskCount, _content, false);
   }
+
+   function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
+
 }
